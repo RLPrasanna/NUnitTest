a0bf4f08b95fa89757cea55409cfba052a31b636
Prasanna Kumar <prasanna.r1234@gmail.com>
2025-04-30 22:51:57 +0530

    Add NUnit test project and update related files
    This commit introduces a new NUnit test project with several test files, including `JuiceHandlerTest.cs` for testing the `IJuiceHandler` implementation. The `.gitignore` file has been updated to exclude specific build directories. The project file `NUnitTest.Test.csproj` now includes necessary package references and properties for testing. Additionally, a basic test class `UnitTest1.cs` has been added, and the solution file `NUnitTest.sln` has been updated to include the new test project.

---
+++ NUnitTest.Test/JuiceHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnitTest.Handlers;
+using NUnitTest.Interfaces;
+using NUnitTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest.Test
+{
+    [TestFixture]
+    internal class JuiceHandlerTest
+    {
+        private IJuiceHandler _juiceHandler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _juiceHandler=new JuiceHandler();
+        }
+
+        [Test]
+        public void GetNoOfPeople_TotalPeopleGreaterThanNotInterested_Positive()
+        {
+            // Arrange
+            Order order = new Order { NoOfPeople = 100, NoOfPeopleNotInterested = 50 };
+
+            // Act
+            _juiceHandler.CreateNewJuice(order);
+            var original = _juiceHandler.GetJuice();
+
+            // Assert
+            Assert.That(original.NoOfFruits, Is.EqualTo(100));
+        }
+    }
+}
